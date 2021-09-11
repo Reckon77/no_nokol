531ba94c569b65d0d678dc4abc4143c3a9bdf174
Reckon Mazumdar <reckonmazumdar12@gmail.com>
2021-09-11 08:28:16 +0530

    added error handling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request, redirect
+from werkzeug.utils import secure_filename
 import os
 from os.path import join, dirname, realpath
 from modules import *
@@ -9,17 +10,26 @@ app = Flask(__name__)
 UPLOADS_PATH = join(dirname(realpath(__file__)), 'static/uploads/')
 app.config['UPLOAD_FOLDER'] = UPLOADS_PATH
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
 
         if request.files:
             file = request.files["file"]
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            path=f"./static/uploads/{file.filename}"
-            data=extractText(path)
-            print(data)
-            return render_template('display.html',data=data)
+            if file.filename == "":
+                return render_template('error.html')
+            if allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                path=f"./static/uploads/{filename}"
+                data=extractText(path)
+                # print(data)
+                return render_template('display.html',data=data)
+            else:
+                return render_template('error.html')
+        else:
+            return render_template('error.html')
     return render_template('index.html')
 
 if __name__ == "__main__":
--- modules.py
+++ modules.py
@@ -9,3 +9,19 @@ def extractText(path):
     for i in range (0,len(data)):
         data[i]="\""+data[i]+"\""
     return data
+
+def allowed_file(filename):
+
+    # We only want files with a . in the filename
+    if not "." in filename:
+        return False
+
+    # Split the extension from the filename
+    ext = filename.rsplit(".", 1)[1]
+
+    # Check if the extension is in allowed
+    allowed=["PDF", "TXT", "DOCX"]
+    if ext.upper() in allowed:
+        return True
+    else:
+        return False
