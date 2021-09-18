94396ef0d162602ab1f6339f7d5614977d7832c2
Reckon Mazumdar <reckonmazumdar12@gmail.com>
2021-09-18 16:48:18 +0530

    select and unselect button added

--- static/display.js
+++ static/display.js
@@ -4,5 +4,37 @@ let plagPercentageSpan=document.getElementById("plagPercent")
 
 let plagCount=parseInt(plagCountSpan.innerText)
 let totalSentences=parseInt(totalSentencesSpan.innerText)
-let perecentage = Math.round((plagCount/totalSentences)*100)
-plagPercentageSpan.innerText=perecentage
+
+const setPercentage=()=>{
+    plagPercentageSpan.innerText=Math.round((plagCount/totalSentences)*100)
+}
+setPercentage()
+
+const plagiarisedSentence=document.querySelectorAll(".plagiarisedSentence")
+const orignalSentence=document.querySelectorAll(".orignalSentence")
+
+for (let i = 0; i < plagiarisedSentence.length; i++) {
+    plagiarisedSentence[i].addEventListener('click', function(e) {
+        plagCount--
+        totalSentences--
+        plagCountSpan.innerText=plagCount
+        totalSentencesSpan.innerText=totalSentences
+        setPercentage()
+      e.currentTarget.parentNode.remove();
+      //this.closest('.single').remove() // in modern browsers in complex dom structure
+      //this.parentNode.remove(); //this refers to the current target element 
+      //e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    }, false);
+  }
+
+  for (let i = 0; i < orignalSentence.length; i++) {
+    orignalSentence[i].addEventListener('click', function(e) {
+        totalSentences--
+        totalSentencesSpan.innerText=totalSentences
+        setPercentage()
+      e.currentTarget.parentNode.remove();
+      //this.closest('.single').remove() // in modern browsers in complex dom structure
+      //this.parentNode.remove(); //this refers to the current target element 
+      //e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    }, false);
+  }
